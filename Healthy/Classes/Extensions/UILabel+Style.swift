7555a500fc8327f043861119da29d1c1c8059c7c
Abdallah Muhammed <andro76987@gmail.com>
2023-05-14 11:56:40 +0300

    Add a style functions to UILabel extensio

--- Healthy/Classes/Extensions/UILabel+Style.swift
+++ Healthy/Classes/Extensions/UILabel+Style.swift
@@ -21,15 +21,36 @@ extension UILabel{
     func applyLabelStyle(style: LabelStyle, fontSize: CGFloat? = nil, weight: UIFont.Weight? = nil){
         switch style{
         case .title:
-            <#code#>
+            applyTitleLabelStyle(fontSize: fontSize, weight: weight)
         case .subtitle:
-            <#code#>
+            applySubtitleLabelStyle(fontSize: fontSize)
         case .description:
-            <#code#>
+            applyDescriptionLabelStyle()
         }
     }
     
-
+    // MARK:  Style attriputes
     
-
+    private func applyTitleLabelStyle(fontSize: CGFloat?, weight: UIFont.Weight?){
+        textColor = UIColor(red: 0.012, green: 0.012, blue: 0.098, alpha: 1)
+        font = UIFont.systemFont(ofSize: fontSize ?? 28, weight: weight ?? .bold)
+        textAlignment = .center
+        numberOfLines = 0
+    }
+    
+    private func applySubtitleLabelStyle(fontSize: CGFloat?){
+        textColor = .secondaryLabel
+        font = UIFont.systemFont(ofSize: fontSize ?? 18, weight: .medium)
+        textAlignment = .center
+        numberOfLines = 0
+    }
+    
+    private func applyDescriptionLabelStyle(){
+        textColor = UIColor(red: 0.282, green: 0.322, blue: 0.373, alpha: 1)
+        font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        textAlignment = .left
+        numberOfLines = 0
+    }
+    
+    
 }
