98876531a7fa5429403f7afe4c5d0288b788d1c4
Abdallah Muhammed <andro76987@gmail.com>
2023-05-15 21:44:39 +0300

    Separate two extensions

--- Healthy/Classes/Extensions/UILabel+Style.swift
+++ Healthy/Classes/Extensions/UILabel+Style.swift
@@ -10,6 +10,7 @@ import UIKit
 extension UILabel{
     
     // MARK:  Stryle tupe
+    
     enum LabelStyle{
         case title
         case subtitle
@@ -29,27 +30,31 @@ extension UILabel{
         }
     }
     
+}
+
+extension UILabel{
+    
     // MARK:  Style attriputes
-        
-        private func applyTitleLabelStyle(fontSize: CGFloat?, weight: UIFont.Weight?){
-            textColor = UIColor(red: 0.012, green: 0.012, blue: 0.098, alpha: 1)
-            font = UIFont.systemFont(ofSize: fontSize ?? 28, weight: weight ?? .bold)
-            textAlignment = .center
-            numberOfLines = 0
-        }
-        
-        private func applySubtitleLabelStyle(fontSize: CGFloat?){
-            textColor = .secondaryLabel
-            font = UIFont.systemFont(ofSize: fontSize ?? 18, weight: .medium)
-            textAlignment = .center
-            numberOfLines = 0
-        }
-        
-        private func applyDescriptionLabelStyle(){
-            textColor = UIColor(red: 0.282, green: 0.322, blue: 0.373, alpha: 1)
-            font = UIFont.systemFont(ofSize: 18, weight: .medium)
-            textAlignment = .left
-            numberOfLines = 0
-        }
+    
+    private func applyTitleLabelStyle(fontSize: CGFloat?, weight: UIFont.Weight?){
+        textColor = UIColor(red: 0.012, green: 0.012, blue: 0.098, alpha: 1)
+        font = UIFont.systemFont(ofSize: fontSize ?? 28, weight: weight ?? .bold)
+        textAlignment = .center
+        numberOfLines = 0
+    }
+    
+    private func applySubtitleLabelStyle(fontSize: CGFloat?){
+        textColor = .secondaryLabel
+        font = UIFont.systemFont(ofSize: fontSize ?? 18, weight: .medium)
+        textAlignment = .center
+        numberOfLines = 0
+    }
+    
+    private func applyDescriptionLabelStyle(){
+        textColor = UIColor(red: 0.282, green: 0.322, blue: 0.373, alpha: 1)
+        font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        textAlignment = .left
+        numberOfLines = 0
+    }
     
 }
