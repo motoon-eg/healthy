098fb76cb48f5edd704c17d2cb67c50904bbdf86
Ahmed Nasr <ahmedkhalil@paymob.com>
2023-05-16 14:23:00 +0300

    refactoring code style in UIButton+Style

--- Healthy/Classes/Extensions/UIButton+Style.swift
+++ Healthy/Classes/Extensions/UIButton+Style.swift
@@ -8,45 +8,62 @@
 import UIKit
 
 
-//MARK: - button style -
+
+// MARK: - Button style
 //
 extension UIButton {
     enum ButtonStyle {
         case primary
         case secondary
         case disable
+        
+        var buttonColor: UIColor {
+            switch self {
+            case .primary:
+                return .black
+            case .secondary:
+                return .green
+            case .disable:
+                return .gray
+            }
+        }
     }
 }
 
 
-//MARK: - apply button style -
+
+// MARK: - Apply button style
 //
 extension UIButton {
     func applyButtonStyle(style: ButtonStyle) {
-        buttonStyleAttributes(style: style)
+        setButtonStyleAttributes(style: style)
     }
 }
 
 
-//MARK: - button style attribute -
+
+// MARK: - set button style attributes
 //
 extension UIButton {
-    private func buttonStyleAttributes(style: ButtonStyle) {
-        switch style {
-        case .primary:
-            backgroundColor = .black
-        case .secondary:
-            backgroundColor = .green
-        case .disable:
-            backgroundColor = .gray
-        }
-        
+    private func setButtonStyleAttributes(style: ButtonStyle) {
+        backgroundColor = style.buttonColor
         tintColor = .white
-        titleLabel?.font = UIFont.systemFont(ofSize: 32)
-        layer.cornerRadius = 12
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .callout, compatibleWith: .none)
+        layer.cornerRadius = Constants.defaultCornerRadius
         layer.masksToBounds = true
         NSLayoutConstraint.activate([
-            heightAnchor.constraint(equalToConstant: 40),
+            heightAnchor.constraint(equalToConstant: Constants.defaultHeight),
         ])
+    }
+}
+
+
+
+//MARK: - Constants
+//
+extension UIButton {
+    enum Constants {
+        static let defaultCornerRadius: CGFloat = 12.0
+        static let defaultHeight: CGFloat = 40.0
     }
 }
