9c182ac2808773b7ac711a128c34a95a46f72b02
Abdallah Muhammed <andro76987@gmail.com>
2023-05-18 11:04:33 +0300

    make names more descriptive

--- Healthy/Classes/Extensions/UILabel+Style.swift
+++ Healthy/Classes/Extensions/UILabel+Style.swift
@@ -1,52 +1,45 @@
-//
-//  UILabel+Style.swift
-//  Healthy
-//
-//  Created by Abdallah on 14/05/2023.
-//
 
 import UIKit
 
 extension UILabel{
     
-    // MARK:  Style tupe
+    // MARK:  Style
     
-    enum LabelStyle{
-        case title
-        case subtitle
-        case description
+    enum Style {
+        case onboardingTitle
+        case onboardingSubtitle
+        case descriptionLabel
         case textFieldTitleLabel
     }
     
-    // MARK:  apply style
+    // MARK:  Apply Style
     
-    func applyLabelStyle(style: LabelStyle){
+    func applyStyle(_ style: Style){
         switch style{
-        case .title:
-            applyTitleLabelStyle()
-        case .subtitle:
-            applySubtitleLabelStyle()
-        case .description:
+        case .onboardingTitle:
+            applyOnboardingTitleLabelStyle()
+        case .onboardingSubtitle:
+            applyOnboardingSubtitleLabelStyle()
+        case .descriptionLabel:
             applyDescriptionLabelStyle()
         case .textFieldTitleLabel:
-            textFieldTitleLabelStyle()
+            applyTextFieldTitleLabelStyle()
         }
-    }    
-    
+    }
 }
 
 extension UILabel{
     
     // MARK:  Style attriputes
     
-    private func applyTitleLabelStyle(){
+    private func applyOnboardingTitleLabelStyle(){
         textColor = UIColor(red: 0.012, green: 0.012, blue: 0.098, alpha: 1)
         font = UIFont.systemFont(ofSize: 28, weight: .bold)
         textAlignment = .center
         numberOfLines = 0
     }
     
-    private func applySubtitleLabelStyle(){
+    private func applyOnboardingSubtitleLabelStyle(){
         textColor = .secondaryLabel
         font = UIFont.systemFont(ofSize: 18, weight: .medium)
         textAlignment = .center
@@ -60,7 +53,7 @@ extension UILabel{
         numberOfLines = 0
     }
     
-    private func textFieldTitleLabelStyle(){
+    private func applyTextFieldTitleLabelStyle(){
         textColor = UIColor(red: 0.012, green: 0.012, blue: 0.098, alpha: 1)
         font = UIFont.systemFont(ofSize: 20, weight: .medium)
         textAlignment = .left
