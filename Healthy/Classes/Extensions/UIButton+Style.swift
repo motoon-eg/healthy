b6ce7fb341daadbfa1845694c2b6bf216eabf1fb
Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
2023-05-18 21:32:58 +0300

    feat: [HL-5] Add UIButton Style Extension  (#13)
    * add button style extension
    
    * refactoring code in UIButton+Style
    
    * refactoring code in applyButtonStyle func in UIButton+Style extension
    
    * refactoring code style in UIButton+Style
    
    * refactoring code style
    
    * refactoring applyButtonStyle func.
    
    * Add generated accent color
    
    ---------
    
    Co-authored-by: ahmednasr <ahmed.nasr.saieed@gmail.com>
    Co-authored-by: Ahmed Nasr <ahmedkhalil@paymob.com>

---
+++ Healthy/Classes/Extensions/UIButton+Style.swift
@@ -0,0 +1,52 @@
+
+
+import UIKit
+
+
+// MARK: - Button style
+//
+extension UIButton {
+    enum ButtonStyle {
+        case primary
+        case secondary
+        case disable
+        
+        var buttonColor: UIColor {
+            switch self {
+            case .primary:
+                return .black
+            case .secondary:
+                return .green
+            case .disable:
+                return .gray
+            }
+        }
+    }
+}
+
+
+// MARK: - Apply button style
+//
+extension UIButton {
+    func applyButtonStyle(_ style: ButtonStyle) {
+        backgroundColor = style.buttonColor
+        tintColor = .white
+        // TODO: [HL-16] Add global fonts
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .callout, compatibleWith: .none)
+        layer.cornerRadius = Constants.defaultCornerRadius
+        layer.masksToBounds = true
+        let widthConstraint = heightAnchor.constraint(equalToConstant: Constants.defaultHeight)
+        widthConstraint.priority = .defaultHigh
+        widthConstraint.isActive = true
+    }
+}
+
+
+// MARK: - Constants
+//
+private extension UIButton {
+    enum Constants {
+        static let defaultCornerRadius: CGFloat = 12.0
+        static let defaultHeight: CGFloat = 40.0
+    }
+}
