a38e09156eee838cc3b0080afe3a5f5e8578278b
Abdallah Muhammed <andro76987@gmail.com>
2023-05-19 21:12:05 +0300

    Changes All Labels to new Green design

--- Healthy/Classes/Extensions/UILabel+Style.swift
+++ Healthy/Classes/Extensions/UILabel+Style.swift
@@ -1,83 +1,107 @@
 import UIKit
 
 extension UILabel {
-
+    
     // MARK: Style
-
+    
     enum Style {
-        case onboardingTitle
-        case onboardingSubtitle
-        case descriptionLabel
-        case textFieldTitleLabel
+        case splashHeaderLabel
+        case splashTitle
+        case splashSubtitle
+        case signinTitle
+        case signinSubtitle
+        case signupTitle
+        case signupSubtitle
     }
-
+    
     // MARK: Apply Style
-
+    
     func applyStyle(_ style: Style) {
         switch style {
-        case .onboardingTitle:
-            applyOnboardingTitleLabelStyle()
-        case .onboardingSubtitle:
-            applyOnboardingSubtitleLabelStyle()
-        case .descriptionLabel:
-            applyDescriptionLabelStyle()
-        case .textFieldTitleLabel:
-            applyTextFieldTitleLabelStyle()
+        case .splashHeaderLabel:
+            applySplashHeaderLabelStyle()
+        case .splashTitle:
+            applySplashTitleLabelStyle()
+        case .splashSubtitle:
+            applySplashSubtitleLabelStyle()
+        case .signinTitle:
+            applySigninTitleLabelStyle()
+        case .signinSubtitle:
+            applySigninSubtitleLabelStyle()
+        case .signupTitle:
+            applySignupTitleLabelStyle()
+        case .signupSubtitle:
+            applySignupSubtitleLabelStyle()
         }
     }
 }
 
 // MARK: Style Attributes
 private extension UILabel {
-    func applyOnboardingTitleLabelStyle() {
-        // TODO: [HL-4] Add global Color
-        textColor = LabelColor.deepBlue
+    func applySplashHeaderLabelStyle() {
+        textColor = .white
         // TODO: [HL-4] Add global fonts
-        font = LableFont.onBoardingTitle
+        font = LabelFont.splashHearderLabel
         textAlignment = .center
-        numberOfLines = 0
     }
-
-    func applyOnboardingSubtitleLabelStyle() {
-        textColor = .secondaryLabel
+    
+    func applySplashTitleLabelStyle() {
+        textColor = .white
         // TODO: [HL-4] Add global fonts
-        font = LableFont.onBoardingSubtitle
+        font = LabelFont.splashTitle
         textAlignment = .center
-        numberOfLines = 0
     }
-
-    func applyDescriptionLabelStyle() {
-        // TODO: [HL-4] Add global Color
+    
+    func applySplashSubtitleLabelStyle() {
+        textColor = .white
+        // TODO: [HL-4] Add global fonts
+        font = LabelFont.splashSubtitle
+        textAlignment = .center
+    }
+    //
+    func applySigninTitleLabelStyle() {
+        textColor = .black
+        // TODO: [HL-4] Add global fonts
+        font = LabelFont.signinTitle
+        textAlignment = .left
+    }
+    
+    func applySigninSubtitleLabelStyle() {
         textColor = LabelColor.slateGray
         // TODO: [HL-4] Add global fonts
-        font = LableFont.description
+        font = LabelFont.signinSubtitle
         textAlignment = .left
-        numberOfLines = 0
     }
-
-    func applyTextFieldTitleLabelStyle() {
-        // TODO: [HL-4] Add global Color
-        textColor = LabelColor.midnightBlue
+    
+    func applySignupTitleLabelStyle() {
+        textColor = .black
         // TODO: [HL-4] Add global fonts
-        font = LableFont.textFieldTitle
+        font = LabelFont.signupTitle
         textAlignment = .left
-        numberOfLines = 0
+    }
+    
+    func applySignupSubtitleLabelStyle() {
+        textColor =  LabelColor.slateGray
+        // TODO: [HL-4] Add global fonts
+        font = LabelFont.signupSubtitle
+        textAlignment = .left
     }
 }
 
 // MARK: - Colors
 
 private enum LabelColor {
-    static let deepBlue = UIColor(red: 0.012, green: 0.012, blue: 0.098, alpha: 1)
-    static let midnightBlue = UIColor(red: 0.039, green: 0.145, blue: 0.200, alpha: 1)
-    static let slateGray = UIColor(red: 0.282, green: 0.322, blue: 0.373, alpha: 1)
+    static let slateGray = UIColor(red: 0.071, green: 0.071, blue: 0.071, alpha: 1)
 }
 
 // MARK: - Fonts
 
-private enum LableFont {
-    static let onBoardingTitle = UIFont.systemFont(ofSize: 28, weight: .bold)
-    static let onBoardingSubtitle = UIFont.systemFont(ofSize: 18, weight: .medium)
-    static let description = UIFont.systemFont(ofSize: 18, weight: .medium)
-    static let textFieldTitle = UIFont.systemFont(ofSize: 20, weight: .medium)
+private enum LabelFont {
+    static let splashHearderLabel = UIFont.systemFont(ofSize: 18, weight: .semibold)
+    static let splashTitle = UIFont.systemFont(ofSize: 50, weight: .semibold)
+    static let splashSubtitle = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let signinTitle = UIFont.systemFont(ofSize: 30, weight: .semibold)
+    static let signinSubtitle = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let signupTitle = UIFont.systemFont(ofSize: 20, weight: .semibold)
+    static let signupSubtitle = UIFont.systemFont(ofSize: 11, weight: .regular)
 }
