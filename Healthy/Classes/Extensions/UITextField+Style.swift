a1b94999aaf47cfa271e6143d3f60ac1eab9923a
ahmednasr <ahmed.nasr.saieed@gmail.com>
2023-05-20 19:53:36 +0300

    configure form text field

---
+++ Healthy/Classes/Extensions/UITextField+Style.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+// MARK: - TextField style
+//
+extension UITextField {
+    enum TextFieldStyle {
+        case primary
+    }
+}
+
+// MARK: - Apply textfield style
+//
+extension UITextField {
+    func applyTextFieldStyle(_ style: TextFieldStyle) {
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: Constants.defaultHeight),
+        ])
+        
+        layer.cornerRadius = Constants.defaultCornerRadius
+        layer.borderWidth = Constants.defaultBorderWidth
+        layer.borderColor = Constants.defaultBorderColor.cgColor
+        
+        setLeftPaddingPoints()
+        setRightPaddingPoints()
+        
+        layer.masksToBounds = true
+    }
+}
+
+// MARK: - set right / left padding to text field
+//
+extension UITextField {
+    private func setLeftPaddingPoints(_ amount: CGFloat = Constants.defaultPadding) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    private func setRightPaddingPoints(_ amount: CGFloat = Constants.defaultPadding) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
+
+// MARK: - Constants
+//
+private extension UITextField {
+    enum Constants {
+        static let defaultCornerRadius: CGFloat = 10.0
+        static let defaultHeight: CGFloat = 55.0
+        static let defaultPadding: CGFloat = 12.0
+        static let defaultBorderWidth: CGFloat = 1.5
+        static let defaultBorderColor: UIColor = UIColor(red: 0.851, green: 0.851, blue: 0.851, alpha: 1)
+    }
+}
+
+
--- Healthy/Classes/ReusableViews/FormTextField/FormTextField.swift
+++ Healthy/Classes/ReusableViews/FormTextField/FormTextField.swift
@@ -6,11 +6,14 @@ class FormTextField: UIView {
     // MARK: - Outlets
     //
     
-    @IBOutlet weak private(set) var contentView: UIView!
-    @IBOutlet weak private(set) var titleLabel: UILabel!
-    @IBOutlet weak private(set) var inputTextField: UITextField!
-    @IBOutlet weak private(set) var errorLabel: UILabel!
+    @IBOutlet weak private var contentView: UIView!
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var inputTextField: UITextField!
+    @IBOutlet weak private var errorLabel: UILabel!
     
+    // MARK: - Properties
+    //
+    var text: ((String)->())?
     
     // MARK: - init
     //
@@ -19,6 +22,7 @@ class FormTextField: UIView {
         super.init(frame: frame)
         initView()
     }
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         initView()
@@ -29,5 +33,46 @@ class FormTextField: UIView {
         addSubview(contentView)
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        setupDesign()
+        bindToTextFieldsChanges()
+    }
+}
+
+// MARK: - configure
+//
+
+extension FormTextField {
+    func configure(title: String, placeholder: String?, type: UIKeyboardType) {
+        self.titleLabel.text = title
+        self.inputTextField.placeholder = placeholder
+        self.inputTextField.keyboardType = type
+    }
+    
+    func setError(_ errorText: String) {
+        self.errorLabel.isHidden = false
+        self.errorLabel.text = errorText
+    }
+}
+
+// MARK: - Private Handlers
+//
+
+extension FormTextField {
+    private func setupDesign() {
+        titleLabel.applyStyle(.textFieldTitleLabel)
+        inputTextField.applyTextFieldStyle(.primary)
+        
+        // waiting for design of error label style
+        //MARK: - feat: [HL-4] Add UI label styling new design
+        errorLabel.textColor = .red
+    }
+    
+    private func bindToTextFieldsChanges() {
+        inputTextField.addTarget(self, action: #selector(textEditingChanged), for: .editingChanged)
+    }
+    
+    @objc private func textEditingChanged() {
+        self.errorLabel.isHidden = true
+        text?(self.inputTextField.text ?? "")
     }
 }
