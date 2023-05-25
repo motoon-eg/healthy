319dcf8bda5a11c007f8f33e90c4c5df73de4978
AhmedNasr <63086808+ahmednasr8898@users.noreply.github.com>
2023-05-26 02:38:57 +0300

    feat: [HL-5] Update button styles (#20)
    * add button style extension
    
    * refactoring code in UIButton+Style
    
    * refactoring code in applyButtonStyle func in UIButton+Style extension
    
    * refactoring code style in UIButton+Style
    
    * refactoring code style
    
    * refactoring applyButtonStyle func.
    
    * Add generated accent color
    
    * add new button style
    
    * fixed Naming issue.
    
    * make all computed properties fileprivate.
    
    ---------
    
    Co-authored-by: Ahmed Nasr <ahmedkhalil@paymob.com>
    Co-authored-by: Ahmed M. Hassan <ahmdmhasn@gmail.com>

--- Healthy/Classes/Extensions/UIButton+Style.swift
+++ Healthy/Classes/Extensions/UIButton+Style.swift
@@ -5,17 +5,35 @@ import UIKit
 extension UIButton {
     enum ButtonStyle {
         case primary
-        case secondary
-        case disable
-
-        var buttonColor: UIColor {
+        case plainGold
+        
+        // TODO: [HL-4] Add global Color
+        fileprivate var buttonBackgroundColor: UIColor? {
             switch self {
             case .primary:
-                return .black
-            case .secondary:
-                return .green
-            case .disable:
-                return .gray
+                return ButtonColors.lightGreen
+            default:
+                return nil
+            }
+        }
+        
+        // TODO: [HL-4] Add global Color
+        fileprivate var buttonTextColor: UIColor? {
+            switch self {
+            case .primary:
+                return .white
+            case .plainGold:
+                return ButtonColors.gold
+            }
+        }
+        
+        // TODO: [HL-16] Add global fonts
+        fileprivate var buttonFont: UIFont? {
+            switch self {
+            case .primary:
+                return UIFont.preferredFont(forTextStyle: .callout, compatibleWith: .none)
+            case .plainGold:
+                return UIFont.preferredFont(forTextStyle: .subheadline, compatibleWith: .none)
             }
         }
     }
@@ -25,15 +43,14 @@ extension UIButton {
 //
 extension UIButton {
     func applyButtonStyle(_ style: ButtonStyle) {
-        backgroundColor = style.buttonColor
-        tintColor = .white
-        // TODO: [HL-16] Add global fonts
-        titleLabel?.font = UIFont.preferredFont(forTextStyle: .callout, compatibleWith: .none)
+        backgroundColor = style.buttonBackgroundColor
+        titleLabel?.font = style.buttonFont
+        tintColor = style.buttonTextColor
         layer.cornerRadius = Constants.defaultCornerRadius
         layer.masksToBounds = true
-        let widthConstraint = heightAnchor.constraint(equalToConstant: Constants.defaultHeight)
-        widthConstraint.priority = .defaultHigh
-        widthConstraint.isActive = true
+        let heightConstraint = heightAnchor.constraint(equalToConstant: Constants.defaultHeight)
+        heightConstraint.priority = .defaultHigh
+        heightConstraint.isActive = true
     }
 }
 
@@ -41,7 +58,16 @@ extension UIButton {
 //
 private extension UIButton {
     enum Constants {
-        static let defaultCornerRadius: CGFloat = 12.0
+        static let defaultCornerRadius: CGFloat = 10.0
         static let defaultHeight: CGFloat = 40.0
+    }
+}
+
+// MARK: - Button Colors
+//
+private extension UIButton {
+    enum ButtonColors {
+        static let lightGreen = UIColor(red: 0.071, green: 0.584, blue: 0.459, alpha: 1)
+        static let gold = UIColor(red: 1.000, green: 0.612, blue: 0.000, alpha: 1)
     }
 }
