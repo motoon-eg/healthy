c9d1c30f308a7631d7c5dbf8a41515cc272a1ff1
AFawzy <fawzy3171@gmail.com>
2023-05-26 02:16:14 +0300

    Create CheckboxButton.swift
    Add checkbox button

---
+++ Healthy/Classes/ReusableViews/CheckboxButton.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class CheckboxButton: UIButton {
+    
+    // Images
+    let checkedImage = UIImage.icCheckboxSelected
+    let uncheckedImage = UIImage.icCheckboxNotSelected
+    
+    // Bool property
+    var isChecked: Bool = false {
+        didSet {
+            if isChecked == true {
+                self.setImage(checkedImage, for: UIControl.State.normal)
+            } else {
+                self.setImage(uncheckedImage, for: UIControl.State.normal)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
+        self.isChecked = false
+    }
+    
+    @objc func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
+}
