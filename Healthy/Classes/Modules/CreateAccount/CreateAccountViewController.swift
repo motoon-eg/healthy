98741e9362a6d758ab670e8d81083e17c043ce60
AFawzy <fawzy3171@gmail.com>
2023-05-26 20:11:20 +0300

    Adjust layout to decrease use of constraints
    Adjust spacing and limit use of constraints

--- Healthy/Classes/Modules/CreateAccount/CreateAccountViewController.swift
+++ Healthy/Classes/Modules/CreateAccount/CreateAccountViewController.swift
@@ -2,12 +2,23 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
 
+    @IBOutlet weak var parentVerticalStackView: UIStackView!
+
     @IBOutlet weak var nameTextField: UITextField!
+
     @IBOutlet weak var emailTextField: UITextField!
+
     @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var confirmPasswordContainerView: UIView!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
+
+    @IBOutlet weak var termsAndConditionsHorizontalStackView: UIStackView!
     @IBOutlet weak var checkBoxButton: CheckboxButton!
+    @IBOutlet weak var acceptTermsTextLabel: UILabel!
+
     @IBOutlet weak var signInTextLabel: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
