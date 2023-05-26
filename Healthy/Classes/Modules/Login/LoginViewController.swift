de9ff2f2f34a1cd006b21b384fda55eaaabc915e
Mohamed Atallah <100219531+mhmdatallaa@users.noreply.github.com>
2023-05-26 10:51:28 +0300

    feat: [HL-21] Create Login view + layout ✨ (#23)
    * Initial commit of Login module
    
    * Creat Login view and layout
    
    * Move Login folder to Modules
    
    * Update view constraint
    
    * Rearrange IBOutlets
    
    * fix method name
    
    * Add signup action method
    
    * Update Healthy/Classes/Modules/Login/LoginViewController.swift
    
    Co-authored-by: Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>

---
+++ Healthy/Classes/Modules/Login/LoginViewController.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    // MARK: Outlets
+
+    @IBOutlet private(set) weak var emailTextFieldLabel: UILabel!
+    @IBOutlet private(set) weak var emailTextField: UITextField!
+    @IBOutlet private(set) weak var passwordTextFieldLabel: UILabel!
+    @IBOutlet private(set) weak var passwordTextField: UITextField!
+    @IBOutlet private(set) weak var forgotPasswordButton: UIButton!
+    @IBOutlet private(set) weak var signInButton: UIButton!
+    @IBOutlet private(set) weak var signInWithGoogleButton: UIButton!
+    @IBOutlet private(set) weak var signInWithFacebookButton: UIButton!
+    @IBOutlet private(set) weak var signUpButton: UIButton!
+
+    // MARK: Properties
+
+    private let viewModel: LoginViewModelType
+
+    // MARK: Init
+
+    init(viewModel: LoginViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureAppearance()
+    }
+}
+
+// MARK: - Actions
+
+private extension LoginViewController {
+    @IBAction func didTapSignIn(_ sender: Any) {}
+    @IBAction func didTapSignUp(_ sender: Any) {}
+}
+
+// MARK: - Configurations
+
+private extension LoginViewController {
+    func configureAppearance() {
+        emailTextFieldLabel.applyStyle(.textFieldTitleLabel)
+        passwordTextFieldLabel.applyStyle(.textFieldTitleLabel)
+        signInButton.applyButtonStyle(.primary)
+        signInWithGoogleButton.applyButtonStyle(.secondary)
+        signInWithFacebookButton.applyButtonStyle(.secondary)
+    }
+}
+
+// MARK: - Private Handlers
+
+private extension LoginViewController {}
