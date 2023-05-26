970f8dcf49383950bdaddac8cc3405e61ae0a2be
AFawzy <fawzy3171@gmail.com>
2023-05-26 20:24:27 +0300

    Change access modifier of IBOutlet in signUpButton to private
    Update the access modifiers of IBOutlets to hide the setter from external classes

--- Healthy/Classes/Modules/CreateAccount/CreateAccountViewController.swift
+++ Healthy/Classes/Modules/CreateAccount/CreateAccountViewController.swift
@@ -2,23 +2,23 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
 
-    @IBOutlet weak var parentVerticalStackView: UIStackView!
+    @IBOutlet private (set) weak var parentVerticalStackView: UIStackView!
 
-    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet private (set) weak var nameTextField: UITextField!
 
-    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet private (set) weak var emailTextField: UITextField!
 
-    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet private (set) weak var passwordTextField: UITextField!
 
-    @IBOutlet weak var confirmPasswordContainerView: UIView!
+    @IBOutlet private (set) weak var confirmPasswordContainerView: UIView!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
 
-    @IBOutlet weak var termsAndConditionsHorizontalStackView: UIStackView!
-    @IBOutlet weak var checkBoxButton: CheckboxButton!
-    @IBOutlet weak var acceptTermsTextLabel: UILabel!
+    @IBOutlet private (set) weak var termsAndConditionsHorizontalStackView: UIStackView!
+    @IBOutlet private (set) weak var checkBoxButton: CheckboxButton!
+    @IBOutlet private (set) weak var acceptTermsTextLabel: UILabel!
 
-    @IBOutlet weak var signInTextLabel: UILabel!
-    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet private (set) weak var signInTextLabel: UILabel!
+    @IBOutlet private (set) weak var signUpButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
