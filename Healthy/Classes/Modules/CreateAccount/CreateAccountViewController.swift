fe51755bb7dfba5cecc7d39a27e87839cd8d3649
AFawzy <fawzy3171@gmail.com>
2023-05-26 20:45:14 +0300

    style: Reformat the code

--- Healthy/Classes/Modules/CreateAccount/CreateAccountViewController.swift
+++ Healthy/Classes/Modules/CreateAccount/CreateAccountViewController.swift
@@ -1,30 +1,30 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-
+    
     @IBOutlet private (set) weak var parentVerticalStackView: UIStackView!
-
+    
     @IBOutlet private (set) weak var nameTextField: UITextField!
-
+    
     @IBOutlet private (set) weak var emailTextField: UITextField!
-
+    
     @IBOutlet private (set) weak var passwordTextField: UITextField!
-
+    
     @IBOutlet private (set) weak var confirmPasswordContainerView: UIView!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
-
+    
     @IBOutlet private (set) weak var termsAndConditionsHorizontalStackView: UIStackView!
     @IBOutlet private (set) weak var checkBoxButton: CheckboxButton!
     @IBOutlet private (set) weak var acceptTermsTextLabel: UILabel!
-
+    
     @IBOutlet private (set) weak var signInTextLabel: UILabel!
     @IBOutlet private (set) weak var signUpButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func didTapSignUp(_ sender: Any) {
     }
-
+    
 }
