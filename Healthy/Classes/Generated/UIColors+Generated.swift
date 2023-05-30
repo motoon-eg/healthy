915fea873b51de9adae944792df874bd27a43c3d
AFawzy <fawzy3171@gmail.com>
2023-05-30 20:09:53 +0300

    chore(Colors): Auto generate colors via SwiftGen 🎨
    - Moved the colors to a separate assets bundle.
    - Generate colors using SwiiftGen

---
+++ Healthy/Classes/Generated/UIColors+Generated.swift
@@ -0,0 +1,13 @@
+import UIKit.UIColor
+
+// this is automatic generated file please don't edit it 🗡️
+// MARK: - Colors
+
+// swiftlint:disable force_unwrapping
+extension UIColor {
+
+      static var orange: UIColor {
+        UIColor(named: "orange")!
+      }
+
+  }
