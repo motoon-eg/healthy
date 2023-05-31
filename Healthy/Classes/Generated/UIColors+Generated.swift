486ce16aedc0ba4ed0c3f0e84ec48c97a1edffda
AFawzy <fawzy3171@gmail.com>
2023-05-31 20:40:06 +0300

    style: Fix linting violations 💇🏼‍♂️
    - resolve linting violation in `SceneDelegate.swift` and `HealthyValidationRule.swift`

--- Healthy/Classes/Generated/UIColors+Generated.swift
+++ Healthy/Classes/Generated/UIColors+Generated.swift
@@ -3,86 +3,87 @@ import UIKit.UIColor
 // this is automatic generated file please don't edit it 🗡️
 // MARK: - Colors
 
+// swiftlint:disable force_unwrapping
 extension UIColor {
-    
+
     static var black: UIColor {
         UIColor(named: "Black")!
     }
-    
+
     static var gray1: UIColor {
         UIColor(named: "Gray 1")!
     }
-    
+
     static var gray2: UIColor {
         UIColor(named: "Gray 2")!
     }
-    
+
     static var gray3: UIColor {
         UIColor(named: "Gray 3")!
     }
-    
+
     static var gray4: UIColor {
         UIColor(named: "Gray 4")!
     }
-    
+
     static var primary100: UIColor {
         UIColor(named: "Primary 100")!
     }
-    
+
     static var primary20: UIColor {
         UIColor(named: "Primary 20")!
     }
-    
+
     static var primary40: UIColor {
         UIColor(named: "Primary 40")!
     }
-    
+
     static var primary60: UIColor {
         UIColor(named: "Primary 60")!
     }
-    
+
     static var primary80: UIColor {
         UIColor(named: "Primary 80")!
     }
-    
+
     static var rating: UIColor {
         UIColor(named: "Rating")!
     }
-    
+
     static var secondary100: UIColor {
         UIColor(named: "Secondary 100")!
     }
-    
+
     static var secondary20: UIColor {
         UIColor(named: "Secondary 20")!
     }
-    
+
     static var secondary40: UIColor {
         UIColor(named: "Secondary 40")!
     }
-    
+
     static var secondary60: UIColor {
         UIColor(named: "Secondary 60")!
     }
-    
+
     static var secondary80: UIColor {
         UIColor(named: "Secondary 80")!
     }
-    
+
     static var success: UIColor {
         UIColor(named: "Success")!
     }
-    
+
     static var warningLight: UIColor {
         UIColor(named: "Warning light")!
     }
-    
+
     static var warning: UIColor {
         UIColor(named: "Warning")!
     }
-    
+
     static var white: UIColor {
         UIColor(named: "white")!
     }
-    
+
 }
