7d452f141b16fb19f99cff29e1113844eb203ac4
Mohamed Atallah <mohamed.atallah440@gmail.com>
2023-06-03 01:04:49 +0300

    add adapter file

---
+++ Healthy/Classes/Utilities/Adapters/SignInAdapter/GooglSignInAuthenticatorAdapter.swift
@@ -0,0 +1,24 @@
+import Foundation
+import GoogleSignIn
+
+enum SingInAuthenticationError: Error {
+    case failedToSignIn
+}
+
+final class GooglSignInAuthenticatorAdapter: Authentication {
+
+    // MARK: Authentication Methods
+
+    func performLogin(withPresenting viewController: UIViewController) async throws -> AuthenticatedUser {
+        try await withCheckedThrowingContinuation { continuation in
+            GIDSignIn.sharedInstance.signIn(withPresenting: viewController) { _, error in
+                guard error == nil else {
+                    continuation.resume(throwing: SingInAuthenticationError.failedToSignIn)
+                    return
+                }
+                continuation.resume(returning: AuthenticatedUser())
+            }
+        }
+    }
+
+}
