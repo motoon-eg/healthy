7316d6ad91b754d77498f6acc5d01a26e5047103
Esraa Khaled <esraakh400@gmail.com>
2023-06-02 18:02:10 +0300

    delete unused validator

--- Healthy/Classes/Extensions/String+ValidName.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-extension String {
-
-    var isValidName: Bool {
-       let regEx = "^\\w{7,18}$"
-       let test = NSPredicate(format: "SELF MATCHES %@", regEx)
-       return test.evaluate(with: self)
-    }
-}
