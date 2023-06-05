a44c1383a3001bde1705a4d9b9f3721bf6376281
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2023-06-05 21:19:10 +0300

    Enhance reusable identifier

--- Healthy/Classes/Extensions/UIView+ReusableIdentifier.swift
+++ Healthy/Classes/Extensions/UIView+ReusableIdentifier.swift
@@ -5,6 +5,6 @@ import UIKit
 extension UIView {
     /// returns a String value, which is the class name of the UIView subclass that it is called on.
     static var reusableIdentifier: String {
-        String("\(Self.self)")
+        String(describing: Self.self)
     }
 }
