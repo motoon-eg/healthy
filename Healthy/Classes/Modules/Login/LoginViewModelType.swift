77a885b817277eecc334b979ac7d4c142c338960
AhmedNasr <63086808+ahmednasr8898@users.noreply.github.com>
2023-06-08 23:53:40 +0300

    test: [HL-39] Sign-in page logic <> Unit Testing ✅ 💫. (#47)
    * finished  login view model logic.
    
    * Update LoginViewModelType.swift
    
    * refactoring code style in Login View Model logic.
    
    * change naming convention.
    
    * init login view model test
    
    * make login view model test.
    
    ---------
    
    Co-authored-by: Ahmed Nasr <ahmedkhalil@paymob.com>

--- Healthy/Classes/Modules/Login/LoginViewModelType.swift
+++ Healthy/Classes/Modules/Login/LoginViewModelType.swift
@@ -1,9 +1,16 @@
+import Foundation
+
 typealias LoginViewModelType = LoginViewModelInput & LoginViewModelOutput
 
 protocol LoginViewModelInput {
-
+    func updateEmail(_ text: String)
+    func updatePassword(_ text: String)
+    func login()
 }
 
 protocol LoginViewModelOutput {
-
+    func onLoadingIndicator(state: @escaping (Bool) -> Void)
+    func onErrorMessage(message: @escaping (String) -> Void)
+    func onButtonEnabled(onEnabled: @escaping (Bool) -> Void)
+    func onLoginStatus(status: @escaping (Bool) -> Void)
 }
