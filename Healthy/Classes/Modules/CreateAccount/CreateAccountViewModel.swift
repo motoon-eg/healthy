82f0eb01e59628b1e3fdeb9b248074efd8cb93c1
Esraa Khaled <esraakh400@gmail.com>
2023-06-08 23:29:03 +0300

    fix indentation

--- Healthy/Classes/Modules/CreateAccount/CreateAccountViewModel.swift
+++ Healthy/Classes/Modules/CreateAccount/CreateAccountViewModel.swift
@@ -2,63 +2,63 @@ import Foundation
 
 // MARK: CreateAccountViewModel
 final class CreateAccountViewModel {
-  private var username: String = ""
-  private var email: String = ""
-  private var password: String = ""
-  private var confirmPassword: String = ""
-  private var isChecked: Bool  = false
-  private var onButtonEnabled: (Bool) -> Void = { _ in }
-
+    private var username: String = ""
+    private var email: String = ""
+    private var password: String = ""
+    private var confirmPassword: String = ""
+    private var isChecked: Bool  = false
+    private var onButtonEnabled: (Bool) -> Void = { _ in }
+    
 }
 
 // MARK: CreateAccountViewModelInput
 // TODO: ... add validation on email & Password
 extension CreateAccountViewModel: CreateAccountViewModelInput {
-  func updateUsername(_ text: String) {
-    username = text
-    updateEnabledStateButton()
-  }
-
-  func updateEmail(_ text: String) {
-    email = text
-    updateEnabledStateButton()
-  }
-
-  func updatePassword(_ text: String) {
-    password = text
-    updateEnabledStateButton()
-  }
-
-  func updateConfirmPassword(_ text: String) {
-    confirmPassword = text
-    updateEnabledStateButton()
-  }
-
-  func updateAcceptTermsAndConditions(_ isChecked: Bool) {
-    self.isChecked = isChecked
-    updateEnabledStateButton()
-  }
+    func updateUsername(_ text: String) {
+        username = text
+        updateEnabledStateButton()
+    }
+    
+    func updateEmail(_ text: String) {
+        email = text
+        updateEnabledStateButton()
+    }
+    
+    func updatePassword(_ text: String) {
+        password = text
+        updateEnabledStateButton()
+    }
+    
+    func updateConfirmPassword(_ text: String) {
+        confirmPassword = text
+        updateEnabledStateButton()
+    }
+    
+    func updateAcceptTermsAndConditions(_ isChecked: Bool) {
+        self.isChecked = isChecked
+        updateEnabledStateButton()
+    }
 }
 
 // MARK: LoginViewModelOutput
 extension CreateAccountViewModel: CreateAccountViewModelOutput {
-  func configureButtonEnabled(onEnabled: @escaping (Bool) -> Void) {
-    onButtonEnabled = onEnabled
-    updateEnabledStateButton()
-  }
+    func configureButtonEnabled(onEnabled: @escaping (Bool) -> Void) {
+        onButtonEnabled = onEnabled
+        updateEnabledStateButton()
+    }
 }
 
 // MARK: Private Handlers
 private extension CreateAccountViewModel {
-  func updateEnabledStateButton() {
-      let isUsernameValid = !username.isEmpty
-      let isEmailValid = !email.isEmpty &&  EmailValidator().hasValidValue(email)
-      let isPasswordValid = !password.isEmpty && PasswordValidator().hasValidValue(password)
-      let isConfirmPasswordValid = !confirmPassword.isEmpty && confirmPassword == password
-
-      let isButtonEnabled = isUsernameValid && isEmailValid && isPasswordValid
-          && isConfirmPasswordValid
-          && isChecked
-      onButtonEnabled(isButtonEnabled)
+    func updateEnabledStateButton() {
+        let isUsernameValid = !username.isEmpty
+        let isEmailValid = !email.isEmpty &&  EmailValidator().hasValidValue(email)
+        let isPasswordValid = !password.isEmpty && PasswordValidator().hasValidValue(password)
+        let isConfirmPasswordValid = !confirmPassword.isEmpty && confirmPassword == password
+        
+        let isButtonEnabled = isUsernameValid && isEmailValid && isPasswordValid
+        && isConfirmPasswordValid
+        && isChecked
+        onButtonEnabled(isButtonEnabled)
     }
 }
--- Healthy/Classes/Modules/CreateAccount/CreateAccountviewModelType.swift
+++ Healthy/Classes/Modules/CreateAccount/CreateAccountviewModelType.swift
@@ -4,15 +4,15 @@ typealias CreateAccountviewModelType = CreateAccountViewModelInput & CreateAccou
 
 // MARK: CreateAccountViewModelInput
 protocol CreateAccountViewModelInput {
-  func updateUsername(_ text: String)
-  func updateEmail(_ text: String)
-  func updatePassword(_ text: String)
-  func updateConfirmPassword(_ text: String)
-  // TODO: configure it using combine
-  func updateAcceptTermsAndConditions(_ isChecked: Bool)
+    func updateUsername(_ text: String)
+    func updateEmail(_ text: String)
+    func updatePassword(_ text: String)
+    func updateConfirmPassword(_ text: String)
+    // TODO: configure it using combine
+    func updateAcceptTermsAndConditions(_ isChecked: Bool)
 }
 
 // MARK: CreateAccountViewModelOutput
 protocol CreateAccountViewModelOutput {
-  func configureButtonEnabled(onEnabled: @escaping(Bool) -> Void)
+    func configureButtonEnabled(onEnabled: @escaping(Bool) -> Void)
 }
