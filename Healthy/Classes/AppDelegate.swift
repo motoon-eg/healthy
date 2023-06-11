75a6ec6af9eb0b286fc74e93d78d82278805d2b8
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2023-06-11 21:25:01 +0300

    Fix indentation issues

--- Healthy/Classes/AppDelegate.swift
+++ Healthy/Classes/AppDelegate.swift
@@ -4,31 +4,31 @@ import FBSDKCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         configureGoogleSignin()
-
-                let config = GIDConfiguration(clientID: "500241227951-jfe9f5o8li3l753c2146hqfru8aaa7o5.apps.googleusercontent.com")
+        
+        let config = GIDConfiguration(clientID: "500241227951-jfe9f5o8li3l753c2146hqfru8aaa7o5.apps.googleusercontent.com")
         GIDSignIn.sharedInstance.configuration = config
         GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
             if error != nil || user == nil {
-              // Show the app's signed-out state.
+                // Show the app's signed-out state.
             } else {
-              // Show the app's signed-in state.
+                // Show the app's signed-in state.
             }
-          }
-
-      ApplicationDelegate.shared.application(
-                 application,
-                 didFinishLaunchingWithOptions: launchOptions
-             )
-
+        }
+        
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -37,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration",
                                     sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication,
                      didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
@@ -46,31 +46,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded
         // scenes, as they will not return.
     }
-
+    
 }
 
 // MARK: - Authentication Handler
 
 extension AppDelegate {
     func applicatxion(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-      var flag: Bool = false
-      if  ApplicationDelegate.shared.application(
-                 app,
-                 open: url,
-                 sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-                 annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-      ) {
-        // URl scheme facebook
-        flag = ApplicationDelegate.shared.application(
-                  app,
-                  open: url,
-                  sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-                  annotation: options[UIApplication.OpenURLOptionsKey.annotation])
-      } else {
-        // URL scheme google
-        flag = GIDSignIn.sharedInstance.handle(url)
-      }
-      return flag
+        var flag: Bool = false
+        if  ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        ) {
+            // URl scheme facebook
+            flag = ApplicationDelegate.shared.application(
+                app,
+                open: url,
+                sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+        } else {
+            // URL scheme google
+            flag = GIDSignIn.sharedInstance.handle(url)
+        }
+        return flag
     }
 }
 
