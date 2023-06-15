55663bbefeb56e92ff90ce4e4bae09f0294b8987
Esraa Khaled <esraakh400@gmail.com>
2023-06-15 23:53:47 +0300

    Revert "Delete LoginViewModelMock.swift"
    This reverts commit 07521fb45f8a22917beca52bd5dba08d8f41ef62.

---
+++ HealthyTests/Classes/Modules/Login/Mocks/LoginViewModelMock.swift
@@ -0,0 +1,71 @@
+@testable import Healthy
+import Combine
+
+final class LoginViewModelMock: LoginViewModelType {
+    @Published var email = ""
+    var isLoadingIndicatorPublisher: AnyPublisher<Bool, Never> {
+        $email
+            .map { $0.isEmpty }
+            .eraseToAnyPublisher()
+    }
+
+    var isShowErrorMessagePublisher: AnyPublisher<String, Never> {
+        $email
+            .map { $0.lowercased() }
+            .eraseToAnyPublisher()
+    }
+
+    var isLoginEnabledPublisher: AnyPublisher<Bool, Never> {
+        $email
+            .map { $0.isEmpty }
+            .eraseToAnyPublisher()
+    }
+
+    var isLoginStatusPublisher: AnyPublisher<Bool, Never> {
+        $email
+            .map { $0.isEmpty }
+            .eraseToAnyPublisher()
+    }
+
+    func updateEmail(_ text: String) {}
+
+    func updatePassword(_ text: String) {}
+
+    func onLoadingIndicator(state: @escaping (Bool) -> Void) {}
+
+    func onErrorMessage(message: @escaping (String) -> Void) {}
+
+    func onButtonEnabled(onEnabled: @escaping (Bool) -> Void) {}
+
+    func onLoginStatus(status: @escaping (Bool) -> Void) {}
+
+    // Forget Password Spy
+    private(set) var performForgetPasswordCount: Int = .zero
+    func performForgetPassword() {
+        performForgetPasswordCount += 1
+    }
+
+    // Sign-in Spy
+    private(set) var performSignInCallCount: Int = .zero
+    func performSignIn() {
+        performSignInCallCount += 1
+    }
+
+    // Sign-up Spy
+    private(set) var performSignUpCallCount: Int = .zero
+    func performSignUp() {
+        performSignUpCallCount += 1
+    }
+
+    // Sign-in with Google Spy
+    private(set) var performSignInWithGoogleCallCount: Int = .zero
+    func performSignInWithGoogle() {
+        performSignInWithGoogleCallCount += 1
+    }
+
+    // Sign-with Facebook Spy
+    private(set) var performSignInWithFacebookCallCount: Int = .zero
+    func performSignInWithFacebook() {
+        performSignInWithFacebookCallCount += 1
+    }
+}
