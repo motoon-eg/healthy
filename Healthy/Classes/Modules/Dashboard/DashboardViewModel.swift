786b2ebd6d976a8c0271d11e2de6b28b507b03cd
Ahmed Nasr <ahmedkhalil@paymob.com>
2023-06-16 14:52:00 +0300

    Update DashboardViewModel.swift

--- Healthy/Classes/Modules/Dashboard/DashboardViewModel.swift
+++ Healthy/Classes/Modules/Dashboard/DashboardViewModel.swift
@@ -11,8 +11,8 @@ extension DashboardViewModel: DashboardViewModelInput {}
 // MARK: DashboardViewModelOutput
 
 extension DashboardViewModel: DashboardViewModelOutput {
-  var header: HomeHeaderView.ViewModel {
-        .init(title: "", subtitle: "", imageUrl: URL(string: "www.google.com")!)
+    var header: HomeHeaderView.ViewModel {
+        .init(title: "", subtitle: "", imageUrl: URL(string: "www.google.com"))
     }
 }
 
