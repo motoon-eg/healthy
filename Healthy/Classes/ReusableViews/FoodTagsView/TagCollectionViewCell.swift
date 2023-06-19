ad11615ada331802b03b3190b4c2a9277c77c063
abdallah-7698 <andro7698@gmail.com>
2023-06-19 14:26:12 +0300

    add outlet to collectionView cell add setFoodCategoryName and setSelection func to set from collectionView

--- Healthy/Classes/ReusableViews/FoodTagsView/TagCollectionViewCell.swift
+++ Healthy/Classes/ReusableViews/FoodTagsView/TagCollectionViewCell.swift
@@ -1,17 +1,23 @@
-//
-//  TagCollectionViewCell.swift
-//  Healthy
-//
-//  Created by Abdallah on 19/06/2023.
-//
-
 import UIKit
 
 class TagCollectionViewCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // MARK: Outlets
+
+    @IBOutlet weak var backView: UIView! {
+        didSet {
+            backView.layer.cornerRadius = backView.frame.height / 3
+        }
+    }
+    @IBOutlet weak var foodCategoryName: UILabel!
+
+    func setFoodCategoryName(_ title: String) {
+        foodCategoryName.text = title
+    }
+
+    func setSelection(_ selected: Bool) {
+        backView.backgroundColor = selected ? .red : .clear
+        foodCategoryName.textColor = selected ? .white : .black
     }
 
 }
