b0af74f6f8e73f6e069e49ef16973a9427b344d7
Clara Mounir Adly <claramounir555@gmail.com>
2023-06-20 00:37:40 +0300

    handle changes

--- HealthyTests/Classes/Utilities/Validators/EmailValidatorsTests.swift
+++ HealthyTests/Classes/Utilities/Validators/EmailValidatorsTests.swift
@@ -1,46 +1,58 @@
 import XCTest
 @testable import Healthy
+
 final class EmailValidatorsTests: XCTestCase {
+
     // MARK: - Properties
+
     var validator: EmailValidator!
 
     // MARK: - Lifecycle
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    override func setUp() {
         super.setUp()
             validator = EmailValidator()
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        validator = nil
-            super.tearDown()
-
     }
 
     // MARK: - Tests
 
     func test_emailValidator_whenEmailIsValid_shouldPass() {
-        let validEmail = "calara23@gmail.com"
-        XCTAssertTrue(validator.hasValidValue(validEmail))
-    }
-    func test_emailValidator_whenEmailIsInValid_shouldfailed() {
-        let noAtSymbolEmail = "testexample.com"
-        XCTAssertFalse(validator.hasValidValue(noAtSymbolEmail))
-    }
-    func test_emailValidator_whenEmailDoesNotHaveDomain_shouldfailed() {
-        let noDomain  = "test@"
-        XCTAssertFalse(validator.hasValidValue(noDomain))
-    }
-    func test_emailValidator_whenEmailHasInValidDomain_shouldfailed() {
-        let inValidDomain = "test@example.c"
-        XCTAssertFalse(validator.hasValidValue(inValidDomain))
-    }
-    func test_emailValidator_whenEmailStartWithNumber_shouldfailed() {
-        func test_emailValidator_whenEmailHasInValidDomain_shouldfailed() {
-            let startsWithNumber = "123test@example.com"
-            XCTAssertFalse(validator.hasValidValue(startsWithNumber))
+            // Given
+            let validEmail = "calara23@gmail.com"
+
+            // Then
+            XCTAssertTrue(validator.hasValidValue(validEmail))
         }
 
-    }
+    func test_emailValidator_whenEmailIsInValid_shouldFail() {
+           // Given
+           let noAtSymbolEmail = "testexample.com"
+
+           // Then
+           XCTAssertFalse(validator.hasValidValue(noAtSymbolEmail))
+       }
+
+    func test_emailValidator_whenEmailDoesNotHaveDomain_shouldFail() {
+           // Given
+           let noDomain  = "test@"
+
+           // Then
+           XCTAssertFalse(validator.hasValidValue(noDomain))
+       }
+
+    func test_emailValidator_whenEmailHasInValidDomain_shouldFail() {
+            // Given
+            let inValidDomain = "test@example.c"
+
+            // Then
+            XCTAssertFalse(validator.hasValidValue(inValidDomain))
+        }
+
+    func test_emailValidator_whenEmailStartWithNumbershouldFail() {
+            // Given
+            let startsWithNumber = "123test@example.com"
+
+            // Then
+            XCTAssertFalse(validator.hasValidValue(startsWithNumber))
+        }
 }
--- HealthyTests/Classes/Utilities/Validators/PasswordValidatorTests.swift
+++ HealthyTests/Classes/Utilities/Validators/PasswordValidatorTests.swift
@@ -1,41 +1,57 @@
 import XCTest
 @testable import Healthy
-final class PasswordValidatorTests: XCTestCase {
-    // MARK: - Properties
-    var validator: PasswordValidator!
-    // MARK: - Lifecycle
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        super.setUp()
-            validator = PasswordValidator()
-    }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        validator = nil
-             super.tearDown()
+final class PasswordValidatorTests: XCTestCase {
+
+    // MARK: - Properties
+
+    private var validator: PasswordValidator!
+
+    // MARK: - Lifecycle
+
+    override func setUp() {
+        super.setUp()
+        validator = PasswordValidator()
     }
 
     // MARK: - Tests
 
     func test_PasswordValidator_whenHasValidPassword_shouldPass() {
+        // Given
         let validPassword = "SecurePassword123"
+
+        // Then
         XCTAssertTrue(validator.hasValidValue(validPassword))
     }
-    func test_PasswordValidator_whenHasShortPassword_shouldfailed() {
+
+    func test_PasswordValidator_whenHasShortPassword_shouldFail() {
+        // Given
         let shortPassword = "Abc"
+
+        // Then
         XCTAssertFalse(validator.hasValidValue(shortPassword))
     }
-    func test_PasswordValidator_whenHasLongPassword_shouldfailed() {
+
+    func test_PasswordValidator_whenHasLongPassword_shouldFail() {
+        // Given
         let longPassword = "Abc123Abc123Abc123Abc123"
+
+        // Then
         XCTAssertFalse(validator.hasValidValue(longPassword))
     }
-    func test_PasswordValidator_whenPasswordIsNumericOnly_shouldfailed() {
+
+    func test_PasswordValidator_whenPasswordIsNumericOnly_shouldFail() {
+        // Given
         let numericPassword = "123456789"
+
+        // Then
         XCTAssertFalse(validator.hasValidValue(numericPassword))
     }
-    func test_PasswordValidator_whenPasswordHasLettersOnly_shouldfailed() {
+    func test_PasswordValidator_whenPasswordHasLettersOnly_shouldFail() {
+        // Given
         let lettersPassword = "abhjklol;k"
+
+        // Then
         XCTAssertFalse(validator.hasValidValue(lettersPassword))
     }
 }
