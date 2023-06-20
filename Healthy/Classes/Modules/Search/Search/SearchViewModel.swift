3db70653ea459e0628c1952de3bb456c1a2be67e
Mohamed Atallah <mohamed.atallah440@gmail.com>
2023-06-20 19:37:09 +0300

    Set search view model

--- Healthy/Classes/Modules/Search/Search/SearchViewModel.swift
+++ Healthy/Classes/Modules/Search/Search/SearchViewModel.swift
@@ -1,15 +1,62 @@
-// MARK: SearchViewModel
-
-final class SearchViewModel {}
+import Combine
 
 // MARK: SearchViewModel
 
-extension SearchViewModel: SearchViewModelInput {}
+final class SearchViewModel {
+    private var subscriptions = Set<AnyCancellable>()
+
+    @Published private var searchKeywords = [String]()
+    @Published private var searchFilters = [SearchFilter]()
+    @Published private var recipe = [Recipe]()
+    @Published private var error: Error = SearchError.unknownError
+    @Published private var isEmpty = false
+    @Published private var isLoading = false
+}
+
+// MARK: SearchViewModel
+
+extension SearchViewModel: SearchViewModelInput {
+    func addNewSearchKeyword(_ keyword: String) {
+        searchKeywords.append(keyword)
+    }
+
+    func addNewSearchFilter(_ filter: SearchFilter) { }
+}
 
 // MARK: SearchViewModelOutput
 
-extension SearchViewModel: SearchViewModelOutput {}
+extension SearchViewModel: SearchViewModelOutput {
+    var recipesPublisher: AnyPublisher<[Recipe], Never> {
+        $recipe.eraseToAnyPublisher()
+    }
 
-// MARK: Private Handlers
+    var errorPublisher: AnyPublisher<Error, Never> {
+        $error.eraseToAnyPublisher()
+    }
 
-private extension SearchViewModel {}
+    var isEmptyPublisher: AnyPublisher<Bool, Never> {
+        $isEmpty.eraseToAnyPublisher()
+    }
+
+    var isLoadingPublisher: AnyPublisher<Bool, Never> {
+        $isLoading.eraseToAnyPublisher()
+    }
+}
+
+// MARK: Private Helpers
+
+private extension SearchViewModel {
+
+    // MARK: Search Error
+
+    enum SearchError: Error {
+        case unknownError
+    }
+
+    // MARK: Search states
+
+    enum SearchState {
+        case initial, loading, loadingMore, empty, loaded
+    }
+
+}
--- Healthy/Classes/Modules/Search/Search/SearchViewModelType.swift
+++ Healthy/Classes/Modules/Search/Search/SearchViewModelType.swift
@@ -1,11 +1,23 @@
+import Combine
+
+public enum SearchFilter { }
+
 /// Search Input & Output
 ///
 typealias SearchViewModelType = SearchViewModelInput & SearchViewModelOutput
 
 /// Search ViewModel Input
 ///
-protocol SearchViewModelInput {}
+protocol SearchViewModelInput {
+    func addNewSearchKeyword(_ keyword: String)
+    func addNewSearchFilter(_ filter: SearchFilter)
+}
 
 /// Search ViewModel Output
 ///
-protocol SearchViewModelOutput {}
+protocol SearchViewModelOutput {
+    var recipesPublisher: AnyPublisher<[Recipe], Never> { get }
+    var errorPublisher: AnyPublisher<Error, Never> { get }
+    var isEmptyPublisher: AnyPublisher<Bool, Never> { get }
+    var isLoadingPublisher: AnyPublisher<Bool, Never> { get }
+}
