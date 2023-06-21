3d992c20ac462fb7ceffb278add9321ac95c30a4
claramounir <107275541+claramounir@users.noreply.github.com>
2023-06-21 23:32:49 +0300

    test: [HL-44] Validator unit test (#64)
    * validator unit test
    
    * fix email validator
    
    * linting auto edites
    
    * handle changes
    
    * handle email test case🧪
    
    * Fix indentation
    
    ---------
    
    Co-authored-by: Ahmed M. Hassan <ahmdmhasn@gmail.com>
    Co-authored-by: Ahmed M. Hassan <ahmed.hassan@tempo.fit>

--- Healthy/Classes/Utilities/Validators/EmailValidator.swift
+++ Healthy/Classes/Utilities/Validators/EmailValidator.swift
@@ -3,7 +3,6 @@ import Foundation
 struct EmailValidator: Validator {
     let validationRules: [ValidationRule] = [
         RegexValidationRule(field: L10n.Common.email,
-                            regex: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"),
-        NumbericValidationRule(field: L10n.Common.email)
+                            regex: "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
     ]
 }
