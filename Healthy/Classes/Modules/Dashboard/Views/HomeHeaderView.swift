076ad1ef1481b65c829286a93d8224aabf252689
Abdallah Mohammed <107879186+abdallah-7698@users.noreply.github.com>
2023-06-21 23:58:28 +0300

    feat: [HL-65] Create search collection view cell 🍕🍕 (#74)
    * create SearchCollectionViewCell with configure func to pass data
    Add view model that contains data
    
    * create outlets and connect them to SearchCollectionViewCell
    
    * connect rating view and make give it corner radius
    
    * give view data from view Model
    
    * remove header
    
    * change file location to SearchRecipes module
    add shadow layer to image and fix the design and image
    
    * make gradient layer on func and make configuration on extension
    
    * Add changes in GitHub review
    
    * apply changes in code review
    
    * d
    
    * separate setupGradientLayer from configureLayout
    
    * Squashed commit of the following:
    
    commit f689bbbe6879376b8541b6d5d0704575fc692217
    Author: Ahmed M. Hassan <45182214+ahmdmhasn@users.noreply.github.com>
    Date:   Tue Jun 20 00:06:51 2023 +0300
    
        chore: Add and integrate NewRelic 📈 (#69)
    
        * Add new relic framework
    
        * Integrate new relic
    
        * Add run script
    
    ---------
    
    Co-authored-by: abdallah-7698 <andro7698@gmail.com>

--- Healthy/Classes/Modules/Dashboard/Views/HomeHeaderView.swift
+++ Healthy/Classes/Modules/Dashboard/Views/HomeHeaderView.swift
@@ -1,14 +1,14 @@
 import UIKit
 
 final class HomeHeaderView: UIView {
-    
+
     // MARK: Outlets
-    
+
     @IBOutlet private(set) weak var contentView: UIView!
     @IBOutlet private(set) weak var titleLabel: UILabel!
     @IBOutlet private(set) weak var subtitleLabel: UILabel!
     @IBOutlet private(set) weak var userImageView: UIImageView!
-    
+
     // MARK: Init
 
     override init(frame: CGRect) {
@@ -27,17 +27,17 @@ final class HomeHeaderView: UIView {
         loadViewFromNib()
         configureLayout()
     }
-    
+
     private func configureLayout() {
         titleLabel.applyStyle(.signupTitle)
         // TODO: - Waiting for add home subtitle label style.
         subtitleLabel.applyStyle(.signupSubtitle)
     }
-    
+
     func configure(with viewModel: ViewModel) {
         titleLabel.text = viewModel.title
         subtitleLabel.text = viewModel.subtitle
-        
+
         // TODO: - [HT-62] Waiting for adding kingfisher (or any library for handle image cashing).
     }
 }
