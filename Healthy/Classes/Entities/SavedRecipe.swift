a13d087ff5afd0c05e07ef345acc781e19a5f690
Esraa Khaled <esraakh400@gmail.com>
2023-06-23 18:30:23 +0300

    Model for Saved recipe

--- Healthy/Classes/Entities/SavedRecipe.swift
+++ Healthy/Classes/Entities/SavedRecipe.swift
@@ -1,34 +1,34 @@
 import UIKit
 
 struct SavedRecipe: Hashable {
-
+    
     // MARK: - Properties
-
+    
     let id = UUID()
     /// The title of the recipe
     let title: String?
-
+    
     /// The  image of the recipe
-    let recipeImageUrl: UIImage?
-
+    let recipeImageUrl: String?
+    
     /// The rating of the recipe
     let rating: Int?
-
+    
     /// The chef  who posted the recipe
     let chefName: String?
-
-    /// The cooking Time of The recipe
+    
+    ///The cooking Time of The recipe
     let cookingTime: Int?
-
+    
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
-
-    static func == (lhs: SavedRecipe, rhs: SavedRecipe) -> Bool {
+    
+    static func ==(lhs: SavedRecipe, rhs: SavedRecipe) -> Bool {
         return lhs.id == rhs.id
     }
-
-    init(title: String?, recipeImageUrl: UIImage?, rating: Int?, chefName: String?, cookingTime: Int?) {
+    
+    init(title: String?, recipeImageUrl: String?, rating: Int?, chefName: String?, cookingTime: Int?) {
         self.title = title
         self.recipeImageUrl = recipeImageUrl
         self.rating = rating
