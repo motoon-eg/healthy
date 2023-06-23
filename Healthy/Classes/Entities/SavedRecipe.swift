5c598a69eadea65451944b7b2ae60c90e350b007
Esraa Khaled <esraakh400@gmail.com>
2023-06-24 01:01:45 +0300

    fix violation

--- Healthy/Classes/Entities/SavedRecipe.swift
+++ Healthy/Classes/Entities/SavedRecipe.swift
@@ -1,33 +1,33 @@
 import UIKit
 
 struct SavedRecipe: Hashable {
-    
+
     // MARK: - Properties
-    
+
     let id = UUID()
     /// The title of the recipe
     let title: String?
-    
+
     /// The  image of the recipe
     let recipeImageUrl: UIImage?
-    
+
     /// The rating of the recipe
     let rating: Int?
-    
+
     /// The chef  who posted the recipe
     let chefName: String?
-    
-    ///The cooking Time of The recipe
+
+    /// The cooking Time of The recipe
     let cookingTime: Int?
-    
+
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
-    
-    static func ==(lhs: SavedRecipe, rhs: SavedRecipe) -> Bool {
+
+    static func == (lhs: SavedRecipe, rhs: SavedRecipe) -> Bool {
         return lhs.id == rhs.id
     }
-    
+
     init(title: String?, recipeImageUrl: UIImage?, rating: Int?, chefName: String?, cookingTime: Int?) {
         self.title = title
         self.recipeImageUrl = recipeImageUrl
--- Healthy/Classes/Modules/SavedRecipes/SavedRecipesViewController.swift
+++ Healthy/Classes/Modules/SavedRecipes/SavedRecipesViewController.swift
@@ -12,25 +12,25 @@ final class SavedRecipesViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
 
     // MARK: Properties
-    
+
     private let viewModel: SavedRecipesViewModelType
     private var dataSource: UITableViewDiffableDataSource<Section, SavedRecipe>!
     private var subscriptions: Set<AnyCancellable> = []
 
     // MARK: Init
-    
+
     init(viewModel: SavedRecipesViewModelType) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -56,7 +56,7 @@ extension SavedRecipesViewController {
     }
 
     func configureTableViewDataSource() {
-        //TODO:  - Add SavedRecipeCell
+        // TODO:  - Add SavedRecipeCell
     dataSource =
         UITableViewDiffableDataSource<Section, SavedRecipe>(tableView: tableView) { tableView, indexPath, viewModel in
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableViewCell", for: indexPath) as? MyTableViewCell
