8f3a343053a76fbb9aef41dea6126a57fa4a0f71
ahmad ayman mansour <ahmed257ayman@gmail.com>
2023-06-27 18:21:38 +0300

    fix: repalce recordeCustomEvent with NRLogger.Log(..)

--- Healthy/Classes/Utilities/Logger/NewRelicLogger.swift
+++ Healthy/Classes/Utilities/Logger/NewRelicLogger.swift
@@ -2,21 +2,31 @@ import Foundation
 import NewRelic
 
 final class NewRelicLogger: Logging {
+    private func convertToNewRelicLoggerLevel(form level:
+                                              LogLevel) -> UInt32 {
+        switch level {
+        case .debug:
+            return 0
+        case .error:
+            return 1 << 0
+        case .warn:
+            return 1 << 1
+        case .info:
+            return 1 << 2
+        case .verbose:
+            return 1 << 3
+        }
+    }
+
     func log(_ message: String,
              level: LogLevel,
              file: StaticString,
              function: StaticString,
              line: UInt) {
-      
-        let attributes: [String : Any] = [
-            "message": "\(level) - \(file) - \(function) - \(line) - \(message)",
-            "level": level,
-            "file": String(describing: file),
-            "function": String(describing: function),
-            "line": line
-        ]
-        
-        NewRelic.recordCustomEvent("logEvents",
-                                   attributes: attributes)
+        NRLogger.log(convertToNewRelicLoggerLevel(form: level),
+                     inFile: String(describing: file),
+                     atLine: UInt32(line),
+                     inMethod: String(describing: function),
+                     withMessage: String(message))
     }
 }
