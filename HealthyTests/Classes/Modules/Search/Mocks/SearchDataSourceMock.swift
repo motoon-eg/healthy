8fc46a4cf85cc2ceea2ba5900cf9101474ff3384
Mohamed Atallah <mohamed.atallah440@gmail.com>
2023-06-27 15:17:56 +0300

    State gitHub comments

--- HealthyTests/Classes/Modules/Search/Mocks/SearchDataSourceMock.swift
+++ HealthyTests/Classes/Modules/Search/Mocks/SearchDataSourceMock.swift
@@ -3,24 +3,8 @@ import Combine
 @testable import Healthy
 
 final class SearchDataSourceMock: SearchDataSource {
-
-    // Error Mock
-    enum SearchDataSourceMockError: Error {
-        case mockedError
-    }
-
-    private var error: Error?
     var loadRecipesCallBack: () async throws -> [Recipe] = { [] }
-
-    init(error: Error? = nil) {
-        self.error = error
-    }
-
     func loadRecipes() async throws -> [Healthy.Recipe] {
-        if error != nil {
-            throw SearchDataSourceMockError.mockedError
-        }
-
-        return [Recipe(), Recipe()]
+        try await loadRecipesCallBack()
     }
 }
