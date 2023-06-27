8b84842487bf327e20865cf8c8c181025dc1dbe8
Abdelhamid.F.Nasser <abdelhamid.f.nasser@gmail.com>
2023-06-27 10:30:11 +0300

    refactor(NewRecipe): Adjust recipe cell design
    - Add the recipe image on top of the card
    - Adjust the card design and shadow

--- Healthy/Classes/Modules/Home/Views/HomeNewRecipesView/CollectionViewCell/HomeNewRecipeCollectionViewCell.swift
+++ Healthy/Classes/Modules/Home/Views/HomeNewRecipesView/CollectionViewCell/HomeNewRecipeCollectionViewCell.swift
@@ -8,17 +8,15 @@ class HomeNewRecipeCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak private(set) var userImage: UIImageView!
     @IBOutlet weak private(set) var userNameLabel: UILabel!
     @IBOutlet weak private(set) var preparationTimeInMinutesLabel: UILabel!
-    @IBOutlet weak private(set) var userPreparationHorizontalStackView: UIStackView!
     @IBOutlet weak private(set) var preparationTimeHorizontalStackView: UIStackView!
+    @IBOutlet weak var contentBackgroundView: UIView!
     @IBOutlet weak private(set) var cardVerticalStackView: UIStackView!
-
     // MARK: - Lifecycle
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         configureAppearance()
-        userPreparationHorizontalStackView.setCustomSpacing(10, after: preparationTimeHorizontalStackView)
     }
 
     // MARK: - Private Methods
@@ -26,13 +24,17 @@ class HomeNewRecipeCollectionViewCell: UICollectionViewCell {
     private func configureAppearance() {
         recipeNameLabel.applyStyle(.textFieldTitleLabel)
         userNameLabel.applyStyle(.signupSubtitle)
-        preparationTimeInMinutesLabel.applyStyle(.textFieldTitleLabel)
-        configureCardShadow(view: cardVerticalStackView)
+//        preparationTimeInMinutesLabel.applyStyle(.signupSubtitle)
+        configureCardShadow(view: contentBackgroundView)
     }
 
     private func configureCardShadow(view: UIView) {
+        view.cornerRadius = 10.0
+        view.backgroundColor = UIColor.white
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowRadius = 4.0
         view.layer.shadowOpacity = 0.1
-        view.layer.shadowRadius = 20.0
+        view.layer.shadowOffset = .zero
         view.layer.masksToBounds = false
     }
 
