a65c6b9d96d62e309954797a5ccdd5e1ab32ea0e
ahmad ayman mansour <ahmed257ayman@gmail.com>
2023-06-27 14:43:24 +0300

    fix: Apply changes based on reviewer comments

--- Healthy/Classes/Utilities/Logger/Logger.swift
+++ Healthy/Classes/Utilities/Logger/Logger.swift
@@ -1,78 +1,79 @@
 import Foundation
 
 final class Logger {
-    private static let cachLogger = FileSystemLogger()
-    private static let logger: Logging = {
-        class NoOpLogger: Logging {
-            func log(_ message: String,
-                     level: LogLevel,
-                     file: StaticString,
-                     function: StaticString,
-                     line: UInt) {
-                // NO-OP
-            }
-        }
-
+    private static let loggers: [Logging] = {
         if NSClassFromString("XCTest") != nil {
             // Unit-test
-            return NoOpLogger()
+            return []
         }
-
-        return cachLogger
+        return [
+            FileSystemLogger(),
+            NewRelicLogger()
+        ]
     }()
-
+    
     static func logDebug(_ message: String,
                          file: StaticString = #fileID,
                          function: StaticString = #function,
                          line: UInt = #line) {
-        logger.log(message,
-                   level: .debug,
-                   file: file,
-                   function: function,
-                   line: line)
+        loggers.forEach { logger in
+            logger.log(message,
+                       level: .debug,
+                       file: file,
+                       function: function,
+                       line: line)
+        }
     }
-
+    
     static func logInfo(_ message: String,
                         file: StaticString = #fileID,
                         function: StaticString = #function,
                         line: UInt = #line) {
-        logger.log(message,
-                   level: .info,
-                   file: file,
-                   function: function,
-                   line: line)
+        loggers.forEach { logger in
+            logger.log(message,
+                       level: .info,
+                       file: file,
+                       function: function,
+                       line: line)
+        }
     }
-
+    
     static func logWarn(_ message: String,
                         file: StaticString = #fileID,
                         function: StaticString = #function,
                         line: UInt = #line) {
-        logger.log(message,
-                   level: .warn,
-                   file: file,
-                   function: function,
-                   line: line)
+        loggers.forEach { logger in
+            logger.log(message,
+                       level: .warn,
+                       file: file,
+                       function: function,
+                       line: line)
+        }
     }
-
+    
     static func logVerbose(_ message: String,
                            file: StaticString = #fileID,
                            function: StaticString = #function,
                            line: UInt = #line) {
-        logger.log(message,
-                   level: .verbose,
-                   file: file,
-                   function: function,
-                   line: line)
+        loggers.forEach { logger in
+            logger.log(message,
+                       level: .verbose,
+                       file: file,
+                       function: function,
+                       line: line)
+        }
     }
-
+    
     static func logError(_ message: String,
                          file: StaticString = #fileID,
                          function: StaticString = #function,
                          line: UInt = #line) {
-        logger.log(message,
-                   level: .error,
-                   file: file,
-                   function: function,
-                   line: line)
+        loggers.forEach { logger in
+            logger.log(message,
+                       level: .error,
+                       file: file,
+                       function: function,
+                       line: line)
+        }
     }
 }
--- Healthy/Classes/Utilities/Logger/NewRelicLogger.swift
+++ Healthy/Classes/Utilities/Logger/NewRelicLogger.swift
@@ -2,18 +2,21 @@ import Foundation
 import NewRelic
 
 final class NewRelicLogger: Logging {
-    func log(_ message: String, level: LogLevel, file: StaticString, function: StaticString, line: UInt) {
-        
-        let timestamp = DateFormatter.localizedString(from: Date(),
-                                                      dateStyle: .medium,
-                                                      timeStyle: .medium)
+    func log(_ message: String,
+             level: LogLevel,
+             file: StaticString,
+             function: StaticString,
+             line: UInt) {
+      
         let attributes: [String : Any] = [
-            "message": "\(timestamp) - \(level) - \(file) - \(function) - \(line) - \(message)",
+            "message": "\(level) - \(file) - \(function) - \(line) - \(message)",
             "level": level,
             "file": String(describing: file),
             "function": String(describing: function),
             "line": line
         ]
-        NewRelic.recordCustomEvent("logEvents",attributes: attributes)
+        
+        NewRelic.recordCustomEvent("logEvents",
+                                   attributes: attributes)
     }
 }
