0e55ad188a9279ddc11478ef1157b30b0a0e4a51
Mohamed Atallah <mohamed.atallah440@gmail.com>
2023-06-28 04:12:25 +0300

    Configure HeaderSkeletonView file

--- Healthy/Classes/Modules/Dashboard/Views/FoodTagsView/FoodTagCollectionViewCell/FoodTagCollectionViewCell.swift
+++ Healthy/Classes/Modules/Dashboard/Views/FoodTagsView/FoodTagCollectionViewCell/FoodTagCollectionViewCell.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+protocol FoodTagCollectionDelegate: AnyObject {}
 
 class FoodTagCollectionViewCell: UICollectionViewCell {
 
--- Healthy/Classes/Modules/Home/Views/SkeletonViews/HomeHeaderSkeletonView.swift
+++ Healthy/Classes/Modules/Home/Views/SkeletonViews/HomeHeaderSkeletonView.swift
@@ -1,6 +1,32 @@
 import UIKit
 
 class HomeHeaderSkeletonView: UIView {
-    
-}
 
+    // MARK: IBOutlets
+
+    @IBOutlet private(set) weak var contentView: UIView!
+    @IBOutlet private(set) weak var titleLabel: UILabel!
+    @IBOutlet private(set) weak var subtitleLabel: UILabel!
+    @IBOutlet private(set) weak var userImageView: UIImageView!
+
+    // MARK: Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSkeletonView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureSkeletonView()
+    }
+
+    // MARK: Configurations
+
+    private func configureSkeletonView() {
+        titleLabel.startSkeletonView()
+        subtitleLabel.startSkeletonView()
+        userImageView.startSkeletonView()
+    }
+
+}
