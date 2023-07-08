f35472557d4a73953160e156b140e03623f47c31
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2023-07-08 19:45:48 +0300

    Add firebase and the meal base urls

--- Networking/Sources/Networking/Constants.swift
+++ Networking/Sources/Networking/Constants.swift
@@ -8,4 +8,20 @@ enum Constants {
 
         return url
     }()
+
+    static let theMealDB: URL = {
+        guard let url = URL(string: "www.themealdb.com/api/json/v1/1/") else {
+            preconditionFailure("Invalid URL")
+        }
+
+        return url
+    }()
+
+    static let firebaseAuth: URL = {
+        guard let url = URL(string: "https://identitytoolkit.googleapis.com/v1") else {
+            preconditionFailure("Invalid URL")
+        }
+
+        return url
+    }()
 }
