7cdaae98b1022175d1da4cc0a4eeea43fd72e910
Doaa AbuJayyab <djayyab@gmail.com>
2023-07-13 02:41:35 +0300

    FilterByMainIngredientEntitites

---
+++ Healthy/Classes/Entities/FilterByMainIngredient.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+// MARK: - FilterByMainIngredient
+public struct FilterByMainIngredient {
+    let meals: [Meal]
+}
+
+// MARK: - Meal
+public struct Meal {
+    let id: String
+    let meal: String
+    let mealThumb: String
+}
