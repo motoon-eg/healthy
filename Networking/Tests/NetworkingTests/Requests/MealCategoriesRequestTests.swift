ddb63b2cf04fe7fb034b2b197e835e083537cb90
abdallah-7698 <andro7698@gmail.com>
2023-07-18 18:39:41 +0300

    fix error in number of character in one line

--- Networking/Tests/NetworkingTests/Requests/MealCategoriesRequestTests.swift
+++ Networking/Tests/NetworkingTests/Requests/MealCategoriesRequestTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import Networking
 
 final class MealCategoriesRequestTests: XCTestCase {
-
+    
     // MARK: Properties
     
     private var sut: MealCategoriesRequest!
@@ -31,7 +31,10 @@ final class MealCategoriesRequestTests: XCTestCase {
                         "idCategory": "1",
                         "strCategory": "Beef",
                         "strCategoryThumb": "https://www.themealdb.com/images/category/beef.png",
-                        "strCategoryDescription": "Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times.[1] Beef is a source of high-quality protein and essential nutrients.[2]"
+                        "strCategoryDescription": "Beef is the culinary name for meat from\
+                        cattle, particularly skeletal muscle. Humans have been eating beef since\
+                        prehistoric times.[1] Beef is a source of high-quality protein and essential\
+                        nutrients.[2]"
                     }
                 ]
             }
