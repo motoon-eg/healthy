bf3628f6c980ce03935fe648a8e259d975797d2d
Ahmed Attalla <devahmedattalla@gmail.com>
2023-07-20 13:32:37 +0300

    Add Domain Entity of Meal Details

---
+++ Healthy/Classes/Entities/MealDetails.swift
@@ -0,0 +1,6 @@
+// MARK: - Meal Details
+struct MealDetails {
+    let id, name, drinkAlternate, category, area, instructions, thumbnail, tags: String?
+    var ingredients: [String] = []
+    var measures: [String] = []
+}
