447cc9be5308bfca6fb00bbbb9c5e26147c6aa4d
ahmednasr <ahmed.nasr.saieed@gmail.com>
2023-07-25 21:43:27 +0300

    Create RegisterUseCase.swift

---
+++ Healthy/Classes/UseCases/RegisterUseCase.swift
@@ -0,0 +1,30 @@
+import Foundation
+import Factory
+import Domain
+import Networking
+
+extension Container {
+    var registerUseCase: Factory<RegisterUseCase> {
+        Factory(self) {
+            DefaultRegisterUseCase()
+        }
+    }
+}
+
+final class DefaultRegisterUseCase: RegisterUseCase {
+    
+    @Injected(\.networking) private var networking
+    
+    func register(email: String, password: String) async throws -> Domain.User {
+        let request = RegisterRequest(email: email, password: password)
+        
+        do {
+            let response = try await networking.dispatch(request)
+            return User(email: response.email, tokenID: response.tokenId)
+            
+        } catch {
+            throw NSError(domain: "we have error !!!", code: -1)
+        }
+    }
+}
+
