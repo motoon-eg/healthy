d26f6d3a3815c53708fbf4b212450473ce74ddd4
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2023-07-27 02:44:18 +0300

    Configure view controller and layout

--- Healthy/Classes/Modules/Dashboard/DashboardViewController.swift
+++ Healthy/Classes/Modules/Dashboard/DashboardViewController.swift
@@ -1,3 +1,4 @@
+import Combine
 import UIKit
 
 final class DashboardViewController: UIViewController {
@@ -9,10 +10,13 @@ final class DashboardViewController: UIViewController {
     // MARK: Subviews
 
     private let headerView = HomeHeaderView()
+    private let tagsView = FoodTagsView()
+    private let newRecipesView = NewRecipesView()
 
     // MARK: Properties
 
     private let viewModel: DashboardViewModelType
+    private var subscriptions: Set<AnyCancellable> = []
 
     // MARK: Init
 
@@ -32,6 +36,8 @@ final class DashboardViewController: UIViewController {
         super.viewDidLoad()
 
         configureView()
+        configureLayout()
+        configureViewModelOutputs()
     }
 }
 
@@ -43,23 +49,63 @@ extension DashboardViewController {}
 
 extension DashboardViewController {
     func configureView() {
+        // Header
         stackView.addArrangedSubview(headerView)
+
+        // Meal Categories
+        stackView.addArrangedSubview(tagsView)
+
+        // New Recipes
+        stackView.addArrangedSubview(makeSection(
+            title: L10n.Home.NewRecipes.header,
+            content: newRecipesView
+        ))
+
         // Workaround to push the content to the top if the content height is
         // less than the available bounds height.
         stackView.addArrangedSubview(UIView())
+    }
+
+    func configureLayout() {
+        tagsView.heightAnchor
+            .equalTo(60)
+            .priority(.defaultHigh)
+        newRecipesView.heightAnchor
+            .equalTo(200)
+            .priority(.defaultHigh)
+    }
+
+    func configureViewModelOutputs() {
+        viewModel.newRecipesPublisher
+            .sink { [weak self] viewModels in
+                self?.newRecipesView.configure(with: viewModels)
+            }
+            .store(in: &subscriptions)
     }
 }
 
 // MARK: - Header Creation
 
 private extension DashboardViewController {
+    /// Create a section view with a title and content.
+    /// - Parameters:
+    ///   - title: The title for the section.
+    ///   - content: The content view to be displayed below the title.
+    /// - Returns: A composed `UIView` containing the title and the specified content view.
     private func makeSection(title: String, content: UIView) -> UIView {
+        // Create a UILabel to display the title
         let titleLabel = UILabel()
         titleLabel.text = title
         titleLabel.applyHeaderBoldStyle()
 
-        let stackView = UIStackView(arrangedSubviews: [titleLabel, content])
-        stackView.axis = .horizontal
+        // Create a UIStackView to hold the title label with appropriate layout
+        let titleStackView = UIStackView(arrangedSubviews: [titleLabel])
+        titleStackView.layoutMargins = UIEdgeInsets(top: 8.0, left: 20.0, bottom: 8.0, right: 20.0)
+        titleStackView.isLayoutMarginsRelativeArrangement = true
+
+        // Create a UIStackView to hold the titleStackView and the specified content view
+        let stackView = UIStackView(arrangedSubviews: [titleStackView, content])
+        stackView.axis = .vertical
         stackView.alignment = .fill
         stackView.spacing = 8.0
 
